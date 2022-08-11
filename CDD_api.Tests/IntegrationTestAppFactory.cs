54d29346ee6237ce03315d43a88296c715206f43
chokchai <chokchai9900@gmail.com>
2022-08-11 13:25:04 +0700

    first commit

---
+++ CDD_api.Tests/IntegrationTestAppFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDD_api.Tests
+{
+    public class IntegrationTestAppFactory<TStartup>
+        : WebApplicationFactory<TStartup>  where TStartup : class
+    {
+    }
+
+    //https://www.youtube.com/watch?v=YU3ohofu6UU
+}
